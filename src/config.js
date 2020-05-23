9d39fed70243674b67f21cd0f2ecb6c3b6b82561
Tyler Alder <tyler.b.alder@gmail.com>
2020-05-23 09:40:54 -0700

    next-level

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://dunder_mifflin@localhost/blogful",
+  API_TOKEN: process.env.API_TOKEN || "d0d1eb60-9643-11ea-bb37-0242ac130002",
+  DB_URL:
+    process.env.DB_URL || "postgresql://dunder_mifflin@localhost/bookmarks",
 };
